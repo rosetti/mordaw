69d04773ae9174076312b405b8a809bb118682e6
Matthew Rumsey <mmr22@kent.ac.uk>
2015-08-19 19:11:51 +0100

    Added Buttons To Project Tab
    +Buttons added and linked to commands

--- Source/GUI/ProjectTab.cpp
+++ Source/GUI/ProjectTab.cpp
@@ -9,6 +9,7 @@
 */
 
 #include "ProjectTab.h"
+#include "../Core/ProjectManager.h"
 
 ProjectTab::ProjectTab(ApplicationCommandManager& commands) : _commands(commands)
 {
@@ -21,9 +22,44 @@ ProjectTab::~ProjectTab()
 
 void ProjectTab::drawButtons()
 {
+	_newProjectButton = new TextButton("New Project", "Create A New Project...");
+	_newProjectButton->setCommandToTrigger(&_commands, ProjectManager::newProject, true);
+
+	_saveProjectButton = new TextButton("Save Project", "Save The Current Project...");
+	_saveProjectButton->setCommandToTrigger(&_commands, ProjectManager::saveProject, true);
+
+	_saveProjectAsButton = new TextButton("Save Project As", "Specify Where To Save The Project...");
+	_saveProjectAsButton->setCommandToTrigger(&_commands, ProjectManager::saveProjectAs, true);
+
+	_openProjectButton = new TextButton("Open Project", "Open A Project...");
+	_openProjectButton->setCommandToTrigger(&_commands, ProjectManager::openProject, true);
+
+	addAndMakeVisible(_newProjectButton);
+	addAndMakeVisible(_saveProjectButton);
+	addAndMakeVisible(_saveProjectAsButton);
+	addAndMakeVisible(_openProjectButton);
+
+	_addTrackButton = new TextButton("Add Track", "Add A New track To The Current Project...");
+	_addTrackButton->setCommandToTrigger(&_commands, ProjectManager::addTrack, true);
+
+	addAndMakeVisible(_addTrackButton);
 }
 
 void ProjectTab::buttonClicked(Button * button)
 {
+}
+
+void ProjectTab::paint(Graphics &)
+{
+}
+
+void ProjectTab::resized()
+{
+	_newProjectButton->setBounds(0, 0, getWidth(), 50);
+	_saveProjectButton->setBounds(0, 50, getWidth(), 50);
+	_saveProjectAsButton->setBounds(0, 100, getWidth(), 50);
+	_openProjectButton->setBounds(0, 150, getWidth(), 50);
+
+	_addTrackButton->setBounds(0, 250, getWidth(), 50);
 }
 
