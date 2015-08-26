8855bb0ec6b5494e2c89ed66888819e9ab073eb4
dtl <dtl@eduDE7F.kent.ac.uk>
2015-08-26 11:41:39 +0100

    Removed extra qualification from header.

--- Source/GUI/TimelineComponent.h
+++ Source/GUI/TimelineComponent.h
@@ -31,7 +31,7 @@ public:
     void setNumberOfClips(int64 numberOfClips);
 
 private:
-    void TimelineComponent::addClips(int64 numberOfClips);
+    void addClips(int64 numberOfClips);
     
     std::vector<TimelineClip*> _clips;
     int64 _mixerOffset;
