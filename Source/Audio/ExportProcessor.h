4d0f2c9c51ce746254559b3ac659ab8a38c9299a
dtl <dtl@eduDE7F.kent.ac.uk>
2015-08-27 21:42:16 +0100

    Recorder and export files;

---
+++ Source/Audio/ExportProcessor.h
@@ -0,0 +1,66 @@
+/*
+  ==============================================================================
+
+    RecordProcessor.h
+    Created: 27 Aug 2015 9:23:06pm
+    Author:  dtl
+
+  ==============================================================================
+*/
+
+#ifndef RECORDPROCESSOR_H_INCLUDED
+#define RECORDPROCESSOR_H_INCLUDED
+
+#include "../JuceLibraryCode/JuceHeader.h"
+
+class ExportProcessor : public AudioProcessor
+{
+public:
+    ExportProcessor();
+    ~ExportProcessor();
+    
+    void prepareToPlay(double sampleRate, int samplesPerBlock) override;
+    void releaseResources() override;
+    
+    void processBlock(AudioSampleBuffer&, MidiBuffer&) override;
+    
+    int getNumParameters() override;
+    float getParameter(int index) override;
+    void setParameter(int index, float newValue) override;
+    void setMuteParameter();
+    const String getParameterName(int index) override;
+    const String getParameterText(int index) override;
+    
+    const String getInputChannelName(int channelIndex) const override;
+    const String getOutputChannelName(int channelIndex) const override;
+    bool isInputChannelStereoPair(int index) const override;
+    bool isOutputChannelStereoPair(int index) const override;
+    
+    double getTailLengthSeconds() const override { return 0.0f;};
+    
+    int getNumPrograms() override { return 0;};
+    int getCurrentProgram() override { return 0;};
+    void setCurrentProgram (int) override {};
+    const String getProgramName (int) override { return "ExportProcessor";};
+    void changeProgramName (int, const String&) {};
+    
+    bool acceptsMidi() const override;
+    bool producesMidi() const override;
+    bool silenceInProducesSilenceOut() const override;
+    
+    void getStateInformation(MemoryBlock& destData) override;
+    void setStateInformation(const void* data, int sizeInBytes) override;
+    
+    const void setID(int ident);
+    const int getID();
+    const String getName() const override { return "ExportProcessor";};
+    AudioProcessorEditor* createEditor() override;
+    bool hasEditor() const override;
+    
+private:
+    float output[];
+};
+
+
+
+#endif  // RECORDPROCESSOR_H_INCLUDED
---
+++ Source/Audio/Recorder.cpp
@@ -0,0 +1,101 @@
+/*
+  ==============================================================================
+
+    Recorder.cpp
+    Created: 27 Aug 2015 3:19:52pm
+    Author:  dtl
+
+  ==============================================================================
+*/
+
+#include "Recorder.h"
+
+Audio::AudioRecorder::AudioRecorder ()
+: backgroundThread ("Recorder Thread"),
+sampleRate (0), nextSampleNum (0), activeWriter (nullptr)
+{
+    backgroundThread.startThread();
+}
+
+Audio::AudioRecorder::~AudioRecorder()
+{
+    stop();
+}
+
+void Audio::AudioRecorder::startRecording (const File& file)
+{
+    stop();
+    
+    if (sampleRate > 0)
+    {
+        // Create an OutputStream to write to our destination file...
+        file.deleteFile();
+        ScopedPointer<FileOutputStream> fileStream (file.createOutputStream());
+        
+        if (fileStream != nullptr)
+        {
+            // Now create a WAV writer object that writes to our output stream...
+            WavAudioFormat wavFormat;
+            AudioFormatWriter* writer = wavFormat.createWriterFor (fileStream, sampleRate, 1, 16, StringPairArray(), 0);
+            
+            if (writer != nullptr)
+            {
+                fileStream.release(); // (passes responsibility for deleting the stream to the writer object that is now using it)
+                
+                // Now we'll create one of these helper objects which will act as a FIFO buffer, and will
+                // write the data to disk on our background thread.
+                threadedWriter = new AudioFormatWriter::ThreadedWriter (writer, backgroundThread, 32768);
+                
+                nextSampleNum = 0;
+                
+                // And now, swap over our active writer pointer so that the audio callback will start using it..
+                const ScopedLock sl (writerLock);
+                activeWriter = threadedWriter;
+            }
+        }
+    }
+}
+
+void Audio::AudioRecorder::stop()
+{
+    {
+        const ScopedLock sl (writerLock);
+        activeWriter = nullptr;
+    }
+    
+    threadedWriter = nullptr;
+}
+
+bool Audio::AudioRecorder::isRecording() const
+{
+    return activeWriter != nullptr;
+}
+
+void Audio::AudioRecorder::audioDeviceAboutToStart (AudioIODevice* device)
+{
+    sampleRate = device->getCurrentSampleRate();
+}
+
+void Audio::AudioRecorder::audioDeviceStopped()
+{
+    sampleRate = 0;
+}
+
+void Audio::AudioRecorder::audioDeviceIOCallback(const float **inputChannelData, int, float **outputChannelData, int numOutputChannels, int numSamples)
+{
+    const ScopedLock sl (writerLock);
+    
+    if (activeWriter != nullptr)
+    {
+        activeWriter->write (inputChannelData, numSamples);
+        
+        // wrap our incomming data, note that this does no allocations or copies, it simply references our input data
+        const AudioSampleBuffer buffer (const_cast<float**> (inputChannelData), 1, numSamples);
+        nextSampleNum += numSamples;
+    }
+    
+    // We need to clear the output buffers, in case they're full of junk..
+    for (int i = 0; i < numOutputChannels; ++i)
+        if (outputChannelData[i] != nullptr)
+            FloatVectorOperations::clear (outputChannelData[i], numSamples);
+}
