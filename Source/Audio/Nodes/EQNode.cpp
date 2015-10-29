af1d3fdfa37a3428fceac2ae03ba707d93ee6830
dtl <dtl@dtls-MBP.default>
2015-10-29 18:49:32 +0000

    Basic structure of processor structure added to EQNode.

--- Source/Audio/Nodes/EQNode.cpp
+++ Source/Audio/Nodes/EQNode.cpp
@@ -9,3 +9,116 @@
 */
 
 #include "EQNode.h"
+
+EQNode::EQNode()
+{
+    
+    lastUIWidth = 400;
+    lastUIHeight = 200;
+    
+    lastPosInfo.resetToDefault();
+    
+}
+
+EQNode::~EQNode()
+{
+}
+
+//==============================================================================
+void EQNode::prepareToPlay (double newSampleRate, int /*samplesPerBlock*/)
+{
+    // Use this method as the place to do any pre-playback
+    // initialisation that you need..
+}
+
+void EQNode::releaseResources()
+{
+
+}
+
+void EQNode::reset()
+{
+    
+}
+
+void EQNode::processBlock (AudioSampleBuffer& buffer, MidiBuffer& midiMessages)
+{
+
+}
+
+//==============================================================================
+AudioProcessorEditor* EQNode::createEditor()
+{
+    return nullptr;
+}
+
+//==============================================================================
+void EQNode::getStateInformation (MemoryBlock& destData)
+{
+    // You should use this method to store your parameters in the memory block.
+    // Here's an example of how you can use XML to make it easy and more robust:
+    
+    // Create an outer XML element..
+    XmlElement xml ("MYPLUGINSETTINGS");
+    
+    // add some attributes to it..
+    xml.setAttribute ("uiWidth", lastUIWidth);
+    xml.setAttribute ("uiHeight", lastUIHeight);
+    xml.setAttribute ("gain", gain->getValue());
+    
+    // then use this helper function to stuff it into the binary blob and return it..
+    copyXmlToBinary (xml, destData);
+}
+
+void EQNode::setStateInformation (const void* data, int sizeInBytes)
+{
+
+}
+
+const String EQNode::getInputChannelName (const int channelIndex) const
+{
+    return String (channelIndex + 1);
+}
+
+const String EQNode::getOutputChannelName (const int channelIndex) const
+{
+    return String (channelIndex + 1);
+}
+
+bool EQNode::isInputChannelStereoPair (int /*index*/) const
+{
+    return true;
+}
+
+bool EQNode::isOutputChannelStereoPair (int /*index*/) const
+{
+    return true;
+}
+
+bool EQNode::acceptsMidi() const
+{
+#if JucePlugin_WantsMidiInput
+    return true;
+#else
+    return false;
+#endif
+}
+
+bool EQNode::producesMidi() const
+{
+#if JucePlugin_ProducesMidiOutput
+    return true;
+#else
+    return false;
+#endif
+}
+
+bool EQNode::silenceInProducesSilenceOut() const
+{
+    return false;
+}
+
+double EQNode::getTailLengthSeconds() const
+{
+    return 0.0;
+}
--- Source/Audio/Nodes/EQNode.h
+++ Source/Audio/Nodes/EQNode.h
@@ -64,10 +64,6 @@ public:
     // A bit of a hacky way to do it, but it's only a demo! Obviously in your own
     // code you'll do this much more neatly..
     
-    // this is kept up to date with the midi messages that arrive, and the UI component
-    // registers with it so it can represent the incoming messages
-    MidiKeyboardState keyboardState;
-    
     // this keeps a copy of the last set of time info that was acquired during an audio
     // callback - the UI component will read this and display it.
     AudioPlayHead::CurrentPositionInfo lastPosInfo;
@@ -79,7 +75,6 @@ public:
     
     // Our parameters
     AudioProcessorParameter* gain;
-    AudioProcessorParameter* delay;
     
 private:
     //==============================================================================
